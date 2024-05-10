4f178a6ecaf58b1e10804c21dc4abd60e4c0e012
Felix Salfelder <felix@salfelder.org>
2024-05-10 00:00:00 +0000

    mfactor in lang_spice

--- lib/e_compon.cc
+++ lib/e_compon.cc
@@ -217,7 +217,6 @@ void COMMON_COMPONENT::print_common_obsolete_callback(OMSTREAM& o, LANGUAGE* lan
   print_pair(o, lang, "tnom", _tnom_c,  _tnom_c.has_hard_value());
   print_pair(o, lang, "dtemp",_dtemp,   _dtemp.has_hard_value());
   print_pair(o, lang, "temp", _temp_c,  _temp_c.has_hard_value());
-//  print_pair(o, lang, "m",    _mfactor, _mfactor.has_hard_value());
 }
 /*--------------------------------------------------------------------------*/
 void COMMON_COMPONENT::set_param_by_index(int i, std::string& Value, int Offset)
