GitMirroring/gnucap

commit cdfbcef08a04e00b5381c7626d89abf2017f555b

restore lang_spice value_name handling

apps/lang_spice.cc
@@ -773,18 +773,13 @@ void LANG_SPICE_BASE::print_args(OMSTREAM& o, const COMPONENT* x)
   if (x->use_obsolete_callback_print()) {
     x->print_args_obsolete_callback(o, this);  //BUG//callback//
   }else{
-    std::string space;
     for (int ii = x->param_count() - 1;  ii >= x->param_count_dont_print();  --ii) {
-      if (!x->param_is_printable(ii)) {
-      }else if(x->param_name(ii) == x->value_name()) {
-	o << x->param_value(ii);
-      }else{
-      }
-    }
-    for (int ii = x->param_count() - 1;  ii >= x->param_count_dont_print();  --ii) {
-      if (!x->param_is_printable(ii)) {
-      }else if(x->param_name(ii) != x->value_name()) {
-	o << " " << x->param_name(ii) << "=";
+      if (x->param_is_printable(ii)) {
+	if ((ii != x->param_count() - 1) || (x->param_name(ii) != x->value_name())) {
+	  // skip name if plain value
+	  o << " " << x->param_name(ii) << "=";
+	}else{
+	}
 	o << x->param_value(ii);
       }else{
       }

tests/==out/d_bjt-pnp.ckt.out
@@ -10,7 +10,7 @@ rb1 ( 1 2 )  27.K
 rb2 ( 2 0 )  1.6K
 rc ( 3 1 )  10.K
 re ( 4 0 )  1.K
-q1 ( 3 2 4 0 )  foo NA( 1.)
+q1 ( 3 2 4 0 )  foo  area=NA( 1.)
 #           v(1)       v(2)       v(3)       v(4)      
  0.         0.         0.         0.         0.        
 -1.        -30.       -1.6657    -21.673    -0.84105   

tests/==out/d_bjt.2.ckt.out
@@ -10,7 +10,7 @@ rb1 ( 1 2 )  27.K
 rb2 ( 2 0 )  1.6K
 rc ( 3 1 )  10.K
 re ( 4 0 )  1.K
-q1 ( 3 2 4 )  foo NA( 1.)
+q1 ( 3 2 4 )  foo  area=NA( 1.)
 #           v(1)       v(2)       v(3)       v(4)      
  0.         0.         0.         0.         0.        
 -1.         30.        1.6657     21.673     0.84105   

tests/==out/d_bjt.ckt.out
@@ -10,7 +10,7 @@ rb1 ( 1 2 )  27.K
 rb2 ( 2 0 )  1.6K
 rc ( 3 1 )  10.K
 re ( 4 0 )  1.K
-q1 ( 3 2 4 0 )  foo NA( 1.)
+q1 ( 3 2 4 0 )  foo  area=NA( 1.)
 #           v(1)       v(2)       v(3)       v(4)      
  0.         0.         0.         0.         0.        
 -1.         30.        1.6657     21.673     0.84105   

tests/==out/d_diode.1.ckt.out
@@ -1,7 +1,7 @@
 simple diode test 
 V1 ( 1 0 )  DC  10.  AC  1.
 R1 ( 1 2 )  100.K
-D1 ( 2 0 )  ddd 1
+D1 ( 2 0 )  ddd  area=1
 .model ddd d ( tnom=NA( 27.) is=10.f rs=0. n=1. tt=0. cjo=1.p pb=1. mj=0.5 egap=1.11 xti=3. kf=0. af=1. fc=0.5 bv=50 ibv=0.001)
 #           v(1)       v(2)       i(V1)      i(R1)      id(D1)     vd(D1)     req(D1)    cd(D1)     z(D1)      zraw(D1)   gd(D1)     z(2)      
  27.        10.        0.59396   -94.061u    94.06u     94.06u     0.59396    274.97     1.5471p    100.K      274.22     0.0036367  274.22    

tests/==out/d_diode.12.ckt.out
@@ -1,7 +1,7 @@
 simple diode test 
 V1 ( 1 0 ) sin offset=NA( 0.) amplitude= 10. frequency= 10.Meg delay=NA( 0.) damping=NA( 0.)  DC -10.  AC  1.
 R1 ( 1 2 )  100.K
-D1 ( 2 0 )  ddd 1.
+D1 ( 2 0 )  ddd  area=1.
 .model ddd d ( tnom=NA( 27.) is=10.f rs=0. n=1. tt=0. cjo=1.p pb=1. mj=0.5 egap=1.11 xti=3. kf=0. af=1. fc=0.5 bv=50 ibv=0.001)
 #           v(1)       v(2)       i(V1)      i(R1)      id(D1)     vd(D1)     req(D1)    cd(D1)     z(D1)      zraw(D1)   gd(D1)     z(2)      
  0.         0.         0.         0.         0.         0.         0.         Inf        0.         1.E+12     1.E+12     0.         1.E+12    

tests/==out/d_diode.3.ckt.out
@@ -1,7 +1,7 @@
 simple diode test 
 V1 ( 1 0 )  DC  10.
 R1 ( 1 2 )  100.K
-D1 ( 2 0 )  ddd 1.
+D1 ( 2 0 )  ddd  area=1.
 .model ddd d ( tnom=NA( 27.) is=10.f rs=0. n=1. tt=0. cjo=1.p pb=1. mj=0.5 egap=1.11 xti=3. kf=0. af=1. fc=0.5 gparallel=1e-5)
 #           v(1)       v(2)       i(V1)      i(R1)      id(D1)     vd(D1)     req(D1)    cap(D1)    z(D1)      zraw(D1)   y(D1)      z(2)      
  27.        10.        0.59234   -94.077u    94.077u    94.077u    0.59234    274.17     1.5448p    100.K      273.42     0.0036473  273.42    

tests/==out/d_diode.4.ckt.out
@@ -1,7 +1,7 @@
 simple diode test 
 V1 ( 1 0 )  DC  10.
 R1 ( 1 2 )  100.K
-D1 ( 2 0 )  ddd 2.
+D1 ( 2 0 )  ddd  area=2.
 .model ddd d ( tnom=NA( 27.) is=10.f rs=0. n=1. tt=0. cjo=1.p pb=1. mj=0.5 egap=1.11 xti=3. kf=0. af=1. fc=0.5)
 #           v(1)       v(2)       i(V1)      i(R1)      id(D1)     vd(D1)     req(D1)    cap(D1)    z(D1)      zraw(D1)   y(D1)      z(2)      
  27.        10.        0.57608   -94.239u    94.239u    94.239u    0.57608    274.45     3.0436p    100.K      273.7      0.0036436  273.7     

commit 2cafd4f675839f21045c4d1578d6b44824075203

cleanup_cc: reverse indexing in COMPONENT interface
- flip COMMON_COMPONENT::*param index args
- fix an issue introduced with cdfbcef08a04e:
   d_bjt.2.ckt.out etc. back to normal
- drop param_id_tag kludge
- indexing now corresponds to visible order

apps/Make.depend
@@ -539,8 +539,7 @@ s_dc.o: s_dc.cc ../include/globals.h ../include/l_dispatcher.h \
  ../include/l_denoise.h ../include/e_compon.h ../include/u_time_pair.h \
  ../include/u_parameter.h ../include/m_expression.h ../include/m_base.h \
  ../include/m_base_vams.h ../include/e_cardlist.h ../include/e_card.h \
- ../include/e_elemnt.h ../include/s__.h ../include/c_comand.h \
- ../include/bm.h
+ ../include/e_elemnt.h ../include/s__.h ../include/c_comand.h
 s_fo.o: s_fo.cc ../include/globals.h ../include/l_dispatcher.h \
  ../include/e_base.h ../include/u_attrib.h ../include/ap.h \
  ../include/md.h ../include/io_trace.h ../include/io_error.h \

apps/lang_spice.cc
@@ -778,12 +778,12 @@ void LANG_SPICE_BASE::print_args(OMSTREAM& o, const COMPONENT* x)
   if (x->use_obsolete_callback_print()) {
     x->print_args_obsolete_callback(o, this);  //BUG//callback//
   }else{
-    for (int ii = x->param_count() - 1;  ii >= x->param_count_dont_print();  --ii) {
+    for (int ii = 0; ii < x->param_count(); ++ii) {
       if (x->param_is_printable(ii)) {
-	if ((ii != x->param_count() - 1) || (x->param_name(ii) != x->value_name())) {
+	if (x->param_name(ii) == x->value_name()) {
 	  // skip name if plain value
-	  o << " " << x->param_name(ii) << "=";
 	}else{
+	  o << " " << x->param_name(ii) << "=";
 	}
 	o << x->param_value(ii);
       }else{

apps/lang_verilog.cc
@@ -196,7 +196,7 @@ void LANG_VERILOG::parse_args_instance(CS& cmd, CARD* x)
       }
     }else{
       // by order
-      for (int Index = x->param_count() - 1;  cmd.is_alnum() || cmd.match1("+-.");  --Index) {
+      for (int Index = 0; cmd.is_alnum() || cmd.match1("+-."); ++Index) {
 	try{
 	  std::string value = cmd.ctos(",)", "", "");
 	  x->set_param_by_index(Index, value, 0/*offset*/);
@@ -748,7 +748,7 @@ void LANG_VERILOG::print_args(OMSTREAM& o, const COMPONENT* x)
     arg_count = INACTIVE;
   }else{
     std::string sep = "";
-    for (int ii = x->param_count() - 1;  ii >= 0;  --ii) {
+    for (int ii = 0; ii < x->param_count(); ++ii) {
       if (x->param_is_printable(ii)) {
 	o << sep;
 	print_attributes(o, x->param_id_tag(ii));

include/e_base.h
@@ -54,7 +54,7 @@ protected: // create and destroy
 public: // tags and attributes (meta-data)
   tag_t			id_tag()const		{return tag_t(this)<<16;}
   tag_t		   port_id_tag(int i)const	{return id_tag()-(i+1);}
-  virtual /*kludge*/ tag_t param_id_tag(int i)const      {return id_tag()+(i+1);}
+  tag_t           param_id_tag(int i)const      {return id_tag()+(i+1);}
 protected:
   const ATTRIB_LIST_p& attributes(tag_t x)const {return _attribs.at(x);}
   ATTRIB_LIST_p&   set_attributes(tag_t x)	{return _attribs[x];}

include/e_card.h
@@ -151,8 +151,6 @@ public:	// obsolete -- do not use in new code
   virtual bool use_obsolete_callback_parse()const {return false;}
   virtual bool use_obsolete_callback_print()const {return false;}
   virtual void print_args_obsolete_callback(OMSTREAM&,LANGUAGE*)const {unreachable();}
-public: // kludge, unnecessary after param re-indexing
-  tag_t     param_id_tag(int i)const override {return id_tag() + param_count() - i;}
 };
 /*--------------------------------------------------------------------------*/
 /*--------------------------------------------------------------------------*/

include/e_paramlist.h
@@ -44,7 +44,7 @@ public:
   int  set_param_by_name(std::string Name, std::string Value) override {
     if(Name[0] != '$'){
       _params.set(Name, Value);
-      return 0; // incomplete?
+      return 0;
     }else{
       return COMMON_COMPONENT::set_param_by_name(Name, Value);
     }

lib/Make.depend
@@ -5,12 +5,7 @@ globals.o: globals.cc ../include/globals.h ../include/l_dispatcher.h \
  ../include/mode.h ../include/io_.h ../include/l_lib.h \
  ../include/u_prblst.h ../include/u_probe.h ../include/l_compar.h \
  ../include/e_cardlist.h ../include/u_status.h ../include/l_timer.h \
- ../include/u_hsparam.h ../include/u_parameter.h \
- ../include/m_expression.h ../include/m_base.h ../include/constant.h \
- ../include/m_base_vams.h ../include/e_cardlist.h \
- ../include/e_paramlist.h ../include/e_compon.h ../include/u_sim_data.h \
- ../include/m_matrix_solver.h ../include/m_matrix.h \
- ../include/u_time_pair.h ../include/e_card.h ../include/patchlev.h
+ ../include/patchlev.h
 m_expression_dump.o: m_expression_dump.cc ../include/m_expression.h \
  ../include/m_base.h ../include/l_lib.h ../include/md.h \
  ../include/io_trace.h ../include/io_error.h ../include/ap.h \
@@ -291,8 +286,7 @@ e_storag.o: e_storag.cc ../include/e_storag.h ../include/e_elemnt.h \
  ../include/ap.h ../include/l_indirect.h ../include/m_cpoly.h \
  ../include/l_denoise.h ../include/e_compon.h ../include/u_time_pair.h \
  ../include/u_parameter.h ../include/m_expression.h ../include/m_base.h \
- ../include/m_base_vams.h ../include/e_cardlist.h ../include/e_card.h \
- ../include/u_hsparam.h ../include/e_paramlist.h
+ ../include/m_base_vams.h ../include/e_cardlist.h ../include/e_card.h
 e_paramlist.o: e_paramlist.cc ../include/e_paramlist.h \
  ../include/e_compon.h ../include/u_sim_data.h ../include/constant.h \
  ../include/md.h ../include/io_trace.h ../include/io_error.h \

lib/e_compon.cc
@@ -353,8 +353,8 @@ int COMMON_COMPONENT::set_param_by_name(std::string Name, std::string Value)
     COMMON_COMPONENT* c = next_common()->clone();
     int idx = c->set_param_by_name(Name, Value);
     attach_next(c);
-    trace1("param idx", idx + param_count());
-    return idx + param_count();;
+    trace2("param idx", Name, idx);
+    return idx;
   }else if (has_parse_params_obsolete_callback()) {untested();
     std::string args(Name + "=" + Value);
     CS cmd(CS::_STRING, args); //obsolete_callback
@@ -769,7 +769,8 @@ int COMPONENT::set_param_by_name(std::string Name, std::string Value)
   if(Name[0] != '$'){
   }else if(!has_common()) {
     attach_common(new HS_PARAM());
-    return mutable_common()->set_param_by_name(Name, Value);
+    int index = mutable_common()->set_param_by_name(Name, Value);
+    return param_count() - index - 1;
   }else{
     if(dynamic_cast<HS_PARAM const*>(common())){ untested();
     }else{
@@ -782,7 +783,7 @@ int COMPONENT::set_param_by_name(std::string Name, std::string Value)
     assert(c);
     int index = c->set_param_by_name(Name, Value);
     attach_common(c);
-    return index;
+    return param_count() - index - 1;
   }else{
     throw Exception_No_Match(Name);
   }
@@ -793,23 +794,25 @@ void COMPONENT::set_param_by_index(int i, std::string& Value, int offset)
   if (has_common()) { untested();
     COMMON_COMPONENT* c = mutable_common()->clone();
     assert(c);
+    int I = param_count() - 1 - i;
     try{ untested();
-    c->set_param_by_index(i, Value, offset);
+      c->set_param_by_index(I, Value, offset);
     }catch(Exception_Too_Many const&){ untested();
       incomplete();
     }
     attach_common(c);
   }else if(i<0){
     throw Exception_Too_Many(-i+1, param_count(), 0);
-  }else{ untested();
-    throw Exception_Too_Many(i, param_count(), 0);
+  }else{
+    throw Exception_Too_Many(i+1, param_count(), 0);
   }
 }
 /*--------------------------------------------------------------------------*/
 bool COMPONENT::param_is_printable(int i)const
 {
   if(has_common()) {
-    return common()->param_is_printable(i);
+    int I = param_count() - 1 - i;
+    return common()->param_is_printable(I);
   }else{ untested();
     return CARD::param_is_printable(i);
   }
@@ -818,7 +821,8 @@ bool COMPONENT::param_is_printable(int i)const
 std::string COMPONENT::param_name(int i)const
 {
   if(has_common()) {
-    return common()->param_name(i);
+    int I = param_count() - 1 - i;
+    return common()->param_name(I);
   }else{ untested();
     return CARD::param_name(i);
   }
@@ -829,7 +833,8 @@ std::string COMPONENT::param_name(int i, int j)const
   if (j == 0) { untested();
     return param_name(i);
   }else if(has_common()){ untested();
-    return common()->param_name(i, j);
+    int I = param_count() - 1 - i;
+    return common()->param_name(I, j);
   }else if (i >= CARD::param_count()) { untested();
     return "";
   }else{untested();
@@ -840,7 +845,8 @@ std::string COMPONENT::param_name(int i, int j)const
 std::string COMPONENT::param_value(int i)const
 {
   if(has_common()){
-    return common()->param_value(i);
+    int I = param_count() - 1 - i;
+    return common()->param_value(I);
   }else{ untested();
     return CARD::param_value(i);
   }

lib/e_elemnt.cc
@@ -178,7 +178,7 @@ int ELEMENT::set_param_by_name(std::string Name, std::string Value)
 /*--------------------------------------------------------------------------*/
 void ELEMENT::set_param_by_index(int i, std::string& Value, int offset)
 {
-  if(ELEMENT::param_count() - 1 == i){
+  if(i == 0) {
     if(!common() || !common()->has_value()){
       push_value(Value);
     }else{
@@ -193,38 +193,48 @@ bool ELEMENT::param_is_printable(int i)const
 {
   if(has_common()) {
     return COMPONENT::param_is_printable(i);
-  }else if(ELEMENT::param_count() - 1 == i){
+  }else if(i == 0) {
     return true;
   }else{ untested();
-    return COMPONENT::param_is_printable(i);
+    unreachable();
+    return COMPONENT::param_is_printable(i-1);
   }
 }
 /*--------------------------------------------------------------------------*/
 std::string ELEMENT::param_name(int i)const
 {
-  if(ELEMENT::param_count() - 1 == i) {
+  if(has_common()) {
+    return COMPONENT::param_name(i);
+  }else if(i == 0){
     return value_name();
   }else{
-    return COMPONENT::param_name(i);
+    unreachable();
+    return COMPONENT::param_name(i-1);
   }
 }
 /*--------------------------------------------------------------------------*/
 std::string ELEMENT::param_name(int i, int j)const
 {
-  if (j == 0) {
+  if(has_common()) {
+    return COMPONENT::param_name(i, j);
+  }else if (j == 0) {
     return param_name(i);
   }else if (i >= ELEMENT::param_count()) {untested();
     return "";
   }else{ untested();
-    return COMPONENT::param_name(i,j);
+    unreachable();
+    return COMPONENT::param_name(i-1,j);
   }
 }
 /*--------------------------------------------------------------------------*/
 std::string ELEMENT::param_value(int i)const
 {
-  if(ELEMENT::param_count() - 1 == i) {
+  if(has_common() && common()->has_value() ) {
+    return COMPONENT::param_value(i);
+  }else if(i == 0) {
     return value_string();
   }else{
+    unreachable();
     return COMPONENT::param_value(i);
   }
 }

tests/==out/u_attrib.2.gc.out
@@ -35,7 +35,7 @@ endmodule // aaa
 module bbb ((* b *) .e(e),.f(f));
 resistor #((* a *) .r( 100.)) r1 (.p(a),.n(b));
 resistor #(.r(200),(* d *) .$mfactor(2)) r2 (.p(a),.n(b));
-resistor #(.r(200),.$mfactor(2)) r2a (.p(a),.n(b));
+resistor #((* d *) .r(200),.$mfactor(2)) r2a (.p(a),.n(b));
 resistor #(.r( 100.)) r3 (.p(a),.n(b));
 resistor #(.r(200),.$mfactor(2)) r4 (.p(a),.n(b));
 resistor #(.r( 100.)) r5 (.p(a),.n(b));

tests/==out/u_attrib.5.gc.out
@@ -34,7 +34,7 @@ endmodule // aaa
 module bbb ((* b *) .e(e),.f(f));
 resistor #((* a, b *) .r( 100.)) r1 (.p(a),.n(b));
 resistor #(.r(200),(* d, e=f, g,h *) .$mfactor(2)) r2 (.p(a),.n(b));
-resistor #(.r(200),.$mfactor(2)) r2a (.p(a),.n(b));
+resistor #((* d *) .r(200),.$mfactor(2)) r2a (.p(a),.n(b));
 resistor #((* x *) .$mfactor(2)) r2b (.p(a),.n(b));
 resistor #(.r( 100.)) r3 (.p(a),.n(b));
 resistor #(.r(200),.$mfactor(2)) r4 (.p(a),.n(b));