cfac74a90542be88a1bc09e37e1c475725af662d
Felix Salfelder <felix@salfelder.org>
2025-02-18 00:00:00 +0000

    cleanup port assign

--- lib/e_cardlist.cc
+++ lib/e_cardlist.cc
@@ -563,7 +563,7 @@ void CARD_LIST::map_subckt_nodes(const CARD* model, const CARD* owner)
       //assert(model->n_(port).e_() == port+1);
       trace3("ports", port, model->n_(port).e_(), owner->n_(port).t_());
       assert(owner->n_(port).is_connected());
-      node_map[port+1].link_to(owner->n_(port)); // BUG. use assign, but prepare.
+      node_map[port+1] = owner->n_(port);
       assert(node_map[port+1].is_link());
     }
     for (int i=model->net_nodes() + 1; i <= num_nodes_in_subckt; ++i) {
