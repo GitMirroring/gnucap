ea747c6004b42caa76d6917dfc5696e230937c80
Felix Salfelder <felix@salfelder.org>
2025-05-25 00:00:00 +0000

    set internal nodes to output in alloc

--- lib/e_node.cc
+++ lib/e_node.cc
@@ -311,7 +311,7 @@ void node_t::allocate(int u /*, CARD* owner*/)
     // repeat call.
   }else{
   }
-  if(is_node()) {
+  if(is_node() && !is_port()) {
     // done.
     trace3("node_t::allocate is_node", this, &root(), _nnn->short_label());
   }else if(_link==this) {
@@ -333,6 +333,8 @@ void node_t::allocate(int u /*, CARD* owner*/)
     NODE* nn = new LOGIC_NODE(flat_number);
     nn->set_owner(nullptr);
     set_own(nn);
+    set_output();
+    assert(is_node() && !is_port());
   }else{
     trace2("node_t::allocate no allocate", _index, u);
   }
