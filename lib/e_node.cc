5837c667555ace3832526dcc594d54cbc38e10a0
Felix Salfelder <felix@salfelder.org>
2025-05-29 00:00:00 +0000

    tighten assertion

--- lib/e_node.cc
+++ lib/e_node.cc
@@ -131,7 +131,7 @@ node_t& node_t::operator=(node_t&& p)
 node_t& node_t::operator=(NODE* n)
 {
   assert(n);
-  assert(!_link || _link == this);
+  assert(!_link || !_nnn);
   // clear();
   if(!_nnn){
     _own = false;
