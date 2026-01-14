866a9f73cf17665aa3d58d40c978501d454b5470
Felix Salfelder <felix@salfelder.org>
2026-01-14 00:00:00 +0000

    modelgen: fix MODEL_CARD::precalc_last
    need to set non-structural values in _last.
    this fixes sweeps.
    
    kludge: just call precalc_first for now
    side effect: duplicate warnings

--- modelgen/mg_out_common.cc
+++ modelgen/mg_out_common.cc
@@ -167,10 +167,21 @@ static void make_common_operator_compare(std::ofstream& out, const Device& d)
     "  assert(p);\n";
 
   out <<
-	 "  if(intptr_t s = intptr_t(_sdp) - intptr_t(p->_sdp)){\n"
+    "  intptr_t s;\n"
+    "  if((s = intptr_t(_sdp) - intptr_t(p->_sdp))){\n"
     "    return (s < 0)?-1:1;\n"
     "  }else{\n"
     "  }\n";
+  for (Args_List::const_iterator
+       p = d.circuit().args_list().begin();
+       p != d.circuit().args_list().end();
+       ++p) {
+    out<<
+      "  if((s = intptr_t(_" << (**p).name() << ") - intptr_t(p->_" << (**p).name() << "))) {\n"
+      "    return (s < 0)?-1:1;\n"
+      "  }else{\n"
+      "  }\n";
+  }
 
   std::string sep;
   if(d.common().calculated().size()){
