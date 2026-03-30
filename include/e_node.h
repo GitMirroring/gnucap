1f5987c2061a69162a04618e9b8669fc554f0171
Felix Salfelder <felix@salfelder.org>
2026-03-30 00:00:00 +0000

    avoid ground node in rank

--- include/e_node.h
+++ include/e_node.h
@@ -134,7 +134,9 @@ private:
   node_t*       link() { assert(!_nnn || !_link); return _link; }
   node_t const* link()const { untested();return _link;}
 private: // union find
-  int rank()const {if(_nnn == &ground_node)return 2;return !!_nnn;} // TODO: hierarchy.
+  int rank()const {if(_m == 0 && _nnn){assert(_nnn==&ground_node); return 2;} else{ 
+    assert(_nnn!=&ground_node);
+    return !!_nnn;}} // TODO: hierarchy.
   int inc_rank()const {return 0;} // TODO
   friend node_t* root(node_t const*);
   friend int     rank(node_t const*);
